bf99f29f4133d0d99955a5dbea459bde0820c5e2
Thiago Rossi <thiagoroossii1@gmail.com>
2025-10-08 04:11:10 -0300

    feat: Stylizes Small Victory Card
    -  Stylized VictorySmallCard with Tailwind CSS and React Bits components

--- kpopdle-frontend/src/components/VictoryCard/VictoryCardSmall.tsx
+++ kpopdle-frontend/src/components/VictoryCard/VictoryCardSmall.tsx
@@ -2,10 +2,12 @@
 import type { GuessedIdolData } from "../../interfaces/gameInterfaces";
 import GlassSurface from "../GlassSurface";
 import trophyIcon from "../../assets/icons/trophy.svg";
+import InstagramLogo from "../../assets/icons/instagram.svg";
+import TwitterLogo from "../../assets/icons/twitter.svg";
 import { X } from 'lucide-react';
 
 
-<X size={20} color="transparent" strokeWidth={0.5} absoluteStrokeWidth />
+
 
 interface VictoryCardSmallProps {
     onClose?: () => void;
@@ -47,8 +49,8 @@ return (
                 {/* Icons Container */}
                 <div className="relative w-full sm:h-25 mb-2">
                     <div className="absolute flex items-center justify-center top-0 right-4 sm:w-10 sm:h-10 rounded-full bg-transparent mt-2.5">
-                        <button className="flex items-center justify-center hover:scale-115 w-full h-full transform duration-500 hover:brightness-110" onClick={onClose}>
-                            <X size={20} color="black" strokeWidth={3} absoluteStrokeWidth className="sm:w-5 sm:h-5 opacity-50 hover:opacity-100 transform duration-500" />
+                        <button className="flex items-center justify-center hover:scale-115 w-full h-full transform duration-500 hover:brightness-110 transform-gpu" onClick={onClose}>
+                            <X size={20} color="black" strokeWidth={3} absoluteStrokeWidth className="sm:w-5 sm:h-5 opacity-50 hover:opacity-100 transform duration-500 transform-gpu" />
                         </button>
                     </div>
                     
@@ -75,7 +77,7 @@ return (
                                 className="bg-white text-transparent bg-clip-text drop-shadow-xl/50">
                                     You guessed it in 
                                 </span> <span 
-                                className="bg-[#ce757a] bg-clip-text text-transparent drop-shadow-xl/50 brightness-120 hover:scale-125 hover:brightness-150 hover:cursor-default">
+                                className="bg-[#ce757a] bg-clip-text text-transparent drop-shadow-xl/50 brightness-120 hover:brightness-150 hover:cursor-default">
                                     {`${attempts} ${attempts === 1 ? "try" : "tries"}!`}
                                 </span>
                             </p>
@@ -84,15 +86,15 @@ return (
 
                     {/* Idol Container */}
                     <div className="flex w-full items-center justify-center sm:h-[100px] mb-4">
-                        <div className="relative flex items-center justify-center bg-black/20 sm:w-80 sm:h-24 sm:p-5 rounded-[20px]
-                        hover:scale-105 hover:bg-black/50 hover:brightness-110 transform duration-300 shadow-2xl">
+                        <div className="relative flex items-center justify-center bg-white/2 sm:w-80 sm:h-24 sm:p-5 rounded-[20px]
+                        hover:scale-105 hover:bg-black/70 hover:brightness-110 transform duration-300 transform-gpu"> {/* Maybe shadow-2xl or lg */}
 
-                            <div className="absolute left-5 flex items-center justify-center sm:h-20 sm:w-20 bg-transparent rounded-[20px] hover:scale-110 hover:rotate-4 transform duration-500 will-change-transform">
-                                <img src={`http://127.0.0.1:5000${cardInfo.image_path}`} alt="Idol" className="w-12 h-12 sm:w-20 sm:h-20 rounded-[20px] object-cover object-top transform-gpu" />
+                            <div className="absolute left-5 flex items-center justify-center sm:h-20 sm:w-20 bg-transparent rounded-[20px] hover:scale-110 hover:rotate-4 transition-transform duration-500 will-change-transform transform-gpu">
+                                <img src={`http://127.0.0.1:5000${cardInfo.image_path}`} alt="Idol" className="w-full h-full rounded-[20px] object-cover object-top transform-gpu" />
                             </div>
 
                             <div className="ml-20 flex flex-col text-center items-center justify-center w-full gap-0.5">
-                                <p className="font-bold text-base sm:text-[22px] bg-gradient-to-b from-[#b43777] to-[#ce757a] brightness-105 shadow-2xl drop-shadow-2xl text-transparent bg-clip-text">
+                                <p className="font-bold text-base sm:text-[22px] bg-gradient-to-b from-[#b43777] to-[#ce757a] shadow-2xl drop-shadow-2xl text-transparent bg-clip-text transform-gpu backface-visibility-hidden">
                                     {cardInfo.artist_name}
                                 </p>
                                 <p className="text-base sm:text-[16px] leading-tight bg-gradient-to-b from-[#ce757a] to-white brightness-105 text-transparent bg-clip-text">
@@ -105,23 +107,27 @@ return (
                     {/* Stats Container */}
                     <div className="flex w-full items-center justify-center sm:h-20 mb-4">
                         <div className="flex flex-row items-center justify-between sm:w-80 sm:h-20">
-                            <div className="relative flex items-center justify-center text-center bg-[#b4b4b4]/60 sm:w-36 sm:h-20 rounded-[20px]">
+                            <div className="relative flex items-center justify-center text-center bg-white/2 sm:w-36 sm:h-20 rounded-[20px] 
+                            hover:scale-105 hover:bg-black/60 hover:brightness-110 transform duration-300 shadow-2xl transform-gpu">
                                 <div className="flex flex-col items-center justify-center text-center gap-0.5">
-                                    <p className="font-bold sm:text-[18px]">
+                                    <p className="font-bold sm:text-[18px] text-[#ce757a] brightness-105">
                                         {attempts}
                                     </p>
-                                    <p className="text-base sm:text-[16px] leading-tight">
+                                    <p className="text-base sm:text-[16px] leading-tight bg-gradient-to-b from-white to-[#ce757a] brightness-105 
+                                    text-transparent bg-clip-text">
                                         {`${attempts === 1 ? "Attempt" : "Attempts"}`}
                                     </p>
                                 </div>
                             </div>
 
-                            <div className="relative flex items-center justify-center text-center bg-[#b4b4b4]/60 sm:w-36 sm:h-20 rounded-[20px]">
+                            <div className="relative flex items-center justify-center text-center bg-white/2 sm:w-36 sm:h-20 rounded-[20px] 
+                            hover:scale-105 hover:bg-black/60 hover:brightness-110 transform duration-300 shadow-2xl transform-gpu">
                                 <div className="flex flex-col items-center justify-center text-center gap-0.5">
-                                    <p className="font-bold sm:text-[18px]">
+                                    <p className="font-bold sm:text-[18px] text-[#ce757a] brightness-105">
                                         3
                                     </p>
-                                    <p className="text-base sm:text-[16px] leading-tight">
+                                    <p className="text-base sm:text-[16px] leading-tight bg-gradient-to-b from-white to-[#ce757a] brightness-105 
+                                    text-transparent bg-clip-text">
                                         On working... (Streak)
                                     </p>
                                 </div>
@@ -132,24 +138,29 @@ return (
                     {/* Share Container */}
                     <div className="flex w-full items-center justify-center sm:h-29 mb-5">
                         <div className="flex flex-col items-center sm:w-80 sm:h-29">
-                            <button className="relative top-0 flex items-center justify-center text-center bg-[#9f9f9f]/60 sm:w-80 sm:h-13 rounded-[16px] mb-4" onClick={() => {console.log("Success")}}>
+                            <button className="relative top-0 flex items-center justify-center text-center bg-[#000]/80 sm:w-80 sm:h-13 rounded-[16px] mb-4
+                            hover:scale-105 hover:brightness-110 hover:bg-[#000]/0 transform duration-300 shadow-2xl hover:shadow-[0px] hover:cursor-pointer transform-gpu" 
+                            onClick={() => {console.log("Success")}}>
                                 <div className="flex items-center justify-center text-center">
-                                    <p className="text-base font-bold sm:text-[20px] leading-tight">
+                                    <p className="text-base font-bold sm:text-[20px] leading-tight bg-gradient-to-r from-[#b43777] to-[#ce757a] brightness-105
+                                    text-transparent bg-clip-text transform-gpu">
                                         Share Results
                                     </p>
                                 </div>
                             </button>
 
                             <div className="flex flex-row items-center justify-center text-center sm:w-80 sm:h-12 gap-6">
-                                <button className="relative left-0 items-center text-center bg-[#747474]/60 sm:w-12 sm:h-12 rounded-[20px]">
-                                    <div className="flex items-center justify-center text-center">
-                                        <img src="/icons/twitter-icon.png" alt="Twt" className="w-6 h-6 sm:w-9 sm:h-9" />
+                                <button className="relative left-0 items-center text-center bg-gradient-to-r from-black/70 to-black/30 sm:w-12 sm:h-12 rounded-[20px] 
+                                hover:scale-110 hover:brightness-110 hover:cursor-pointer hover:rotate-3 hover:bg-black/100 transform duration-300 transform-gpu">
+                                    <div className="flex w-full h-full items-center justify-center text-center brightness-100 hover:brightness-110 transform duration-300">
+                                        <img src={TwitterLogo} alt="Twt" className="w-6 h-6 sm:w-9 sm:h-9" />
                                     </div>
                                 </button>
 
-                                <button className="relative right-0 items-center text-center bg-[#747474]/60 sm:w-12 sm:h-12 rounded-[20px]">
-                                    <div className="flex items-center justify-center text-center">
-                                        <img src="/icons/instagram-icon.png" alt="Insta" className="w-6 h-6 sm:w-9 sm:h-9" />
+                                <button className="relative right-0 items-center text-center bg-gradient-to-r from-black/30 to-black/70 sm:w-12 sm:h-12 rounded-[20px]
+                                hover:scale-110 hover:brightness-110 hover:cursor-pointer hover:-rotate-3 hover:bg-black/100 transform duration-300 transform-gpu">
+                                    <div className="flex w-full h-full items-center justify-center text-center brightness-100 hover:brightness-110">
+                                        <img src={InstagramLogo} alt="Insta" className="w-6 h-6 sm:w-9 sm:h-9" />
                                     </div>
                                 </button>
                             </div>
@@ -159,10 +170,10 @@ return (
                     {/* Next idol Container */}
                     <div className="flex w-full items-center justify-center sm:h-5">
                         <div className="flex flex-row items-center justify-center text-center sm:w-48 sm:h-5 gap-1">
-                            <p className="text-base text-[16px]">
+                            <p className="text-base text-[16px] text-[#ce757a] brightness-105">
                                 Next idol in 
                             </p>
-                            <p className="text-base font-bold text-[16px]">
+                            <p className="text-base font-bold text-[16px] text-white brightness-105">
                                 {nextReset().formattedTime}
                             </p>
                         </div>
