35d19c388991b3c46555ccbe04f50d7baa8dd33e
Thiago Rossi <thiagoroossii1@gmail.com>
2026-03-26 21:13:15 -0300

    feat: add global notice component and maintenance banner
    - Implements a reusable `GlobalNotices` component for displaying dismissible system-wide announcements.
    
    - This allows for easy communication of upcoming maintenance or database updates to users.
    
    - It will be improved for future uses but is usable for now.

--- kpopit-frontend/src/components/MainLayout/MainLayout.tsx
+++ kpopit-frontend/src/components/MainLayout/MainLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import { getUserAnalytics } from "../../services/api";
+import GlobalNotices from "../GlobalNotices/GlobalNotices";
 
 const MainLayout = () => {
     useEffect(() => {
@@ -18,6 +19,13 @@ const MainLayout = () => {
     return (
         <div className="min-h-screen flex flex-col bg-transparent text-foreground selection:bg-pink-800">
             <NavBar />
+            
+            {/* Maintenance Mode Notice */}
+            {import.meta.env.VITE_GLOBAL_NOTICE === "true" && 
+                <GlobalNotices 
+                    children="Kpopit will enter in maintenance mode soon for database updates." 
+                />
+            }
 
             <main className="flex-1 max-w-screen-2xl mx-auto px-2 sm:px-4 lg:px-8 w-full">
                 <Outlet />
